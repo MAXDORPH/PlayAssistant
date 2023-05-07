5b306e4147c8817734119a01d2195f60f0e44446
ironkrendel <v.a.ryshkov@gmail.com>
2023-05-07 20:10:33 +0300

    Добавил кастомный элемент генератора рандома

---
+++ PlayAssistant/RandomGenerator.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace clown_mega_project
+{
+    /// <summary>
+    /// Логика взаимодействия для Random.xaml
+    /// </summary>
+    public partial class RandomGenerator : UserControl
+    {
+        public RandomGenerator()
+        {
+            InitializeComponent();
+        }
+
+        private void Generate_btn_Click(object sender, RoutedEventArgs e)
+        {
+            Random rand = new Random();
+            int res = 0;
+            int from, to;
+
+            try
+            {
+                from = Convert.ToInt32(From_textbox.Text);
+                to = Convert.ToInt32(To_textbox.Text);
+                Result_textblock.Text = rand.Next(from, to).ToString();
+            }
+            catch {
+                Result_textblock.Text = "Not a number";
+            }
+
+        }
+    }
+}
