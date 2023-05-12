7602ab1a830654d4c69b3e31ec31dd5c0915ca45
ironkrendel <v.a.ryshkov@gmail.com>
2023-05-12 21:32:57 +0300

    Добавил счетчик

---
+++ PlayAssistant/CounterElement.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace clown_mega_project
+{
+    /// <summary>
+    /// Логика взаимодействия для CounterElement.xaml
+    /// </summary>
+    public partial class CounterElement : UserControl
+    {
+        public int value = 0;
+
+        public CounterElement()
+        {
+            InitializeComponent();
+        }
+
+        private void Element_Loaded(object sender, RoutedEventArgs e)
+        {
+            double labelfontsize = Application.Current.MainWindow.Height / 4;
+            double btnfontsize = App.Current.MainWindow.Height / 7;
+            System.Windows.Application.Current.Resources.Remove("LabelFontSize");
+            System.Windows.Application.Current.Resources.Add("LabelFontSize", labelfontsize);
+            System.Windows.Application.Current.Resources.Remove("BtnFontSize");
+            System.Windows.Application.Current.Resources.Add("BtnFontSize", btnfontsize);
+        }
+
+        private void Element_Resized(object sender, SizeChangedEventArgs e)
+        {
+            double labelfontsize = Application.Current.MainWindow.Height / 4;
+            double btnfontsize = App.Current.MainWindow.Height / 7;
+            System.Windows.Application.Current.Resources.Remove("LabelFontSize");
+            System.Windows.Application.Current.Resources.Add("LabelFontSize", labelfontsize);
+            System.Windows.Application.Current.Resources.Remove("BtnFontSize");
+            System.Windows.Application.Current.Resources.Add("BtnFontSize", btnfontsize);
+        }
+
+        private void Minus_btn_Click(object sender, RoutedEventArgs e)
+        {
+            value--;
+            Update_text();
+        }
+
+        private void Reset_btn_Click(object sender, RoutedEventArgs e)
+        {
+            value = 0;
+            Update_text();
+        }
+
+        private void Plus_btn_Click(object sender, RoutedEventArgs e)
+        {
+            value++;
+            Update_text();
+        }
+
+        private void Update_text()
+        {
+            Value_label.Content = value.ToString();
+        }
+    }
+}
