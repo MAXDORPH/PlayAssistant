7b45ee25fe603631f05e6b0b3d8940116cb1cb5f
ironkrendel <82104670+ironkrendel@users.noreply.github.com>
2023-05-14 15:25:12 +0300

    Create GameCreateMenu.xaml.cs

---
+++ PlayAssistant/GameCreateMenu.xaml.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PlayAssistant
+{
+    /// <summary>
+    /// Логика взаимодействия для GameCreateMenu.xaml
+    /// </summary>
+    public partial class GameCreateMenu : UserControl
+    {
+        public GameCreateMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void Back_btn_Click(object sender, RoutedEventArgs e)
+        {
+            ((GameChooseMenu)Application.Current.MainWindow.Content).CloseGameCreate();
+        }
+    }
+}
